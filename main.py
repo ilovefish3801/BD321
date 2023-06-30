4b6f456ca58263dabc8ee96ed22d8d31ef36372c
ilovefish3801 <pipipupi.kom@gmail.com>
2023-06-30 19:02:46 +0300

    a
    a

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 if __name__ == "__main__":
-    first_name = str(input("""Введіть своє ім'я: """))
-    second_name = str(input("Введіть своє прізвище: "))
-    full_name = f"{first_name}  {second_name}".lower()
+    # first_name = str(input("""Введіть своє ім'я: """))
+    # second_name = str(input("Введіть своє прізвище: "))
+    # full_name = f"{first_name}  {second_name}".lower()
+    #
+    # print(full_name.title())
+    # full_name = first_name + second_name
+    # full_name.lower()
+    #
+    # full_name.replace(" ", '')
+    # print(len(full_name))
 
-    print(full_name.title())
-    full_name = first_name + second_name
-    full_name.lower()
+    someArray = [1, "test", True, 1.6, "hey"]
 
-    full_name.replace(" ", '')
-    print(len(full_name))
+    print(someArray[4])
+
+    someArray.append(2)
+
+    print(someArray)
+
+    someArray.pop(1)
+
+    targeteElement = someArray.index(1.6)
+    print(targeteElement)
+    someArray[targeteElement] = "test3"
+    print(someArray)
+
+    someArray.reverse()
+    print(someArray)
+
+    someArray.clear()
+    print(someArray)
