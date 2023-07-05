5d34fa5de9d176e001dd075749dd743d92e03eb5
ilovefish3801 <138194455+ilovefish3801@users.noreply.github.com>
2023-07-05 19:27:38 +0300

    a

--- main.py
+++ main.py
@@ -1,87 +1,79 @@
 if __name__ == "__main__":
-    # first_name = str("John")
-    # second_name = str("Doe")
-    # third_name = str("Patrick")
-    #
-    # user_data = {"name": first_name, "fathersName": third_name, "surname": second_name,
-    #              "pib": f"{second_name.title()} {first_name[0].upper()}. {third_name[0].upper()}.", 'age': 26}
-    #
-    # # print(third_name[::-1])
-    #
-    # print(user_data)
-    #
-    # arr_names = ("John", "Jerry", "Simon")
-    #
-    # arr_surnames = {"Doe", "Lee", "Chan"}
-    #
-    # arr_surnames.add('test')
-    # arr_surnames.remove('Lee')
-    #
-    # print(arr_surnames)
+    # temperature = int(input("Введіть температуру: "))
 
-    users = [
-        {
-            "name": "John",
-            "age": 31
-        },
-        {
-            "name": "Jane",
-            "age": 27,
-            "family": {
-                'mom': {
-                    "name": "Sophie",
-                    "age": 54,
-                },
-                'dad': {
-                    "name": "Johny",
-                    "age": 56,
-                },
-
-            },
-        },
-        {
-            "name": "Jack",
-            "age": 15
-        },
-        {
-            "name": "Jonathan",
-            "age": 38
-        },
-    ]
-
-    # print(users[2]['name'], users[2]['age'])
-    # print(users[1]["family"])
-
-    # temp_age = users[1]['age']
-    # temp_name = users[1]['name']
-    #
-    # if temp_age >= 18:
-    #     print(temp_name, "you are 18 or older congrats !")
-    # elif temp_name <= 18:
-    #     print(temp_name, "you are a child I wont sell you any drinks >:(")
-
-    # degrees = int(input("Введіть температуру: "))
-    #
-    # if degrees > 0:
-    #     print("Тепло вдягнись легше")
-    # elif degrees < 0:
-    #     print("Ух, як холодно. Вдягни щось тепле")
+    # if 10 >= temperature > -10:
+    #     print("Прохолодно")
+    # elif temperature > 10:
+    #     print("Тепло")
     # else:
-    #     print("Вдягнись як хочеш")
+    #     print("Холодно")
 
-    print("""1-Цельсії""")
-    print("""2-Кельвіни""")
-    print("""3-Фарантгейти""")
+    # kisnevo_riven = int(input("Введіть рівень кисню: "))
 
-    theDegrees = int(input("Введіть градуси: "))
-    degrees_Type = int(input("Введіть вид градусу: "))
+    # if kisnevo_riven > 18 and 35 >= temperature >= 25:
+    #     print("Можна відкрити вікно")
+    # else:
+    #     print("Не можна відкривати вікно")
 
-    if degrees_Type == 1:
-        print(theDegrees * 1.8 + 32, "Фарантгейтів")
-        print(theDegrees + 273.15, "Кельвінів")
-    elif degrees_Type == 2:
-        print(theDegrees - 273.15, "Цельсіїв")
-        print((theDegrees - 273.15) * 1.8000 + 32.00, "Фарантгейти")
+    # age = int(input("Скільки вам років ? "))
+    # income = int(input("Який у вас дохід ? "))
+    # employed_input = input("Ви маєте стабільне працевлаштування ? ")
+    # blacklist_input = input("Ви знаходитесь в списку небажаних позичальників ? ")
+    # employed = bool
+    # blacklist = bool
+    #
+    # if employed_input == "так":
+    #     employed = True
+    # else:
+    #     employed = False
+    #
+    # if blacklist_input == "ні":
+    #     blacklist = False
+    # else:
+    #     blacklist = True
+    #
+    # if employed == True and blacklist == False and 18 < age < 65 and income >= 25000:
+    #     print("Вам допустимий кредит")
+    # else:
+    #     print("Вам не допустимий кредит")
+
+    friend_check = input("Ви привели з собою друга ?")
+    friend_credit = bool
+    if friend_check == "так":
+        friend_credit = True
     else:
-        print(theDegrees - 32 / 1.8, "Цельсіїв")
-        print(theDegrees - 32 / 1.8 + 273.75, "Кельвінів")
+        friend_credit = False
+
+    
+
+
+
+
+
+    age = int(input("Скільки вам років ? "))
+    income = int(input("Який у вас дохід ? "))
+    category = " "
+    credit_time = int(input("Введіть час кредиту (в місяцях): "))
+    credit_amount = int(input("Введіть суму кредиту: "))
+    credit_percent = credit_time / 100 * 2
+    credit_sum = credit_amount * credit_percent
+    credit_sum = credit_sum + credit_amount
+    credit_per_month = credit_sum / credit_time
+
+    if 18 <= age <= 25 and income < 20000:
+        category = "Студеньський кредит"
+    elif 26 <= age <= 40 and 20000 < income < 40000:
+        category = "Особистий кредит"
+    elif 41 <= age <= 60 and income > 40000:
+        category = "Поточний кредит"
+    else:
+        category = "Недоступний кредит"
+
+
+
+
+    max_credit = (income * age) / 4
+    print("Максимальний допустимий кредит: ", max_credit)
+    print(category)
+    print(round(credit_per_month, 1))
+
