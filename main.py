44a021f308e7eb2856aeb2f571cfb73214caeab5
ilovefish3801 <pipipupi.kom@gmail.com>
2023-07-07 19:38:10 +0300

    a

--- main.py
+++ main.py
@@ -1,87 +1,113 @@
 if __name__ == "__main__":
-    # first_name = str("John")
-    # second_name = str("Doe")
-    # third_name = str("Patrick")
+    # students = [
+    #     {
+    #         "name": "John",
+    #         "presense": None,
     #
-    # user_data = {"name": first_name, "fathersName": third_name, "surname": second_name,
-    #              "pib": f"{second_name.title()} {first_name[0].upper()}. {third_name[0].upper()}.", 'age': 26}
+    #     },
+    #     {
+    #         "name": "Jerry",
+    #         "presense": None
+    #     },
+    #     {
+    #         "name": "Jane",
+    #         "presense": None
+    #     },
+    # ]
+    # presense_count = 0
     #
-    # # print(third_name[::-1])
+    # for student in students:
+    #     flag = True
+    #     while flag:
+    #         presense_choice = input(f"Чи є {student['name']} на занятті ? (Y/N)")
     #
-    # print(user_data)
+    #         if presense_choice.lower() == "y":
+    #             student['presense'] = True
+    #             presense_count += 1
+    #             flag = False
+    #         elif presense_choice.lower() == "n":
+    #             student['presense'] = False
+    #             flag = False
+    #         if flag:
+    #             print("Ви обрали не вірне значення ! \n\n")
     #
-    # arr_names = ("John", "Jerry", "Simon")
-    #
-    # arr_surnames = {"Doe", "Lee", "Chan"}
-    #
-    # arr_surnames.add('test')
-    # arr_surnames.remove('Lee')
-    #
-    # print(arr_surnames)
+    # print(f"Присутні: {presense_count}. Відсутні: {len(students) - presense_count}")
 
-    users = [
-        {
-            "name": "John",
-            "age": 31
+    # count_new_student = int(input("Скільки студентів хочете додати ? "))
+    # for i in range(count_new_student):
+    #
+    #     name_student = input("""Введіть ім'я нового студента: """)
+    #     frame_student = {
+    #         "name": name_student,
+    #         "presense": None
+    #     }
+    #     students.append(frame_student)
+    #
+    # print(students)
+
+    # a = True
+    # b = 0
+    # while True:
+    #     b += 1
+    #     if b >= 100:
+    #         continue
+    #     elif b >= 105:
+    #         break
+    #     print(b)
+
+    # b = 0
+    # while b <= 100:
+    #     question = int(input("Введіть число яке більше за 100: "))
+    #     if question > 100:
+    #         print("Ти вгадав")
+    #         b = 101
+
+    credit_data = {
+        'user1': {
+            'credit1': 5000,
+            'credit2': 2000,
+            'credit3': 10000
         },
-        {
-            "name": "Jane",
-            "age": 27,
-            "family": {
-                'mom': {
-                    "name": "Sophie",
-                    "age": 54,
-                },
-                'dad': {
-                    "name": "Johny",
-                    "age": 56,
-                },
-
-            },
+        'user2': {
+            'credit1': 3000,
+            'credit2': 1500,
+            'credit3': 8000
         },
-        {
-            "name": "Jack",
-            "age": 15
+        'user3': {
+            'credit1': 7000,
+            'credit2': 0,
+            'credit3': 5000
         },
-        {
-            "name": "Jonathan",
-            "age": 38
-        },
-    ]
-
-    # print(users[2]['name'], users[2]['age'])
-    # print(users[1]["family"])
-
-    # temp_age = users[1]['age']
-    # temp_name = users[1]['name']
+        'user20': {
+            'credit1': 10000,
+            'credit2': 5000,
+            'credit3': 3000
+        }
+    }
+    # credit_one_amount = 0
+    # credit_two_amount = 0
+    # credit_three_amount = 0
+    # for i in credit_data:
+    #     for credit in credit_data[i]:
+    #         if credit == "credit1":
+    #             credit_one_amount += credit_data[i][credit]
+    #         elif credit == "credit2":
+    #             credit_two_amount += credit_data[i][credit]
+    #         elif credit == "credit3":
+    #             credit_three_amount += credit_data[i][credit]
     #
-    # if temp_age >= 18:
-    #     print(temp_name, "you are 18 or older congrats !")
-    # elif temp_name <= 18:
-    #     print(temp_name, "you are a child I wont sell you any drinks >:(")
+    # print(" Заборговані по Кредиту1: ", credit_one_amount)
+    # print(" Заборговані по Кредиту2: ", credit_two_amount)
+    # print(" Заборговані по Кредиту3: ", credit_three_amount)
+    max_debt = 0
+    for i in credit_data:
+        total_debt = 0
+        for credit in credit_data[i]:
+            total_debt += credit_data[i][credit]
 
-    # degrees = int(input("Введіть температуру: "))
-    #
-    # if degrees > 0:
-    #     print("Тепло вдягнись легше")
-    # elif degrees < 0:
-    #     print("Ух, як холодно. Вдягни щось тепле")
-    # else:
-    #     print("Вдягнись як хочеш")
+        if max_debt < total_debt:
+            max_debt = total_debt
+            user_max_debt = i
 
-    print("""1-Цельсії""")
-    print("""2-Кельвіни""")
-    print("""3-Фарантгейти""")
-
-    theDegrees = int(input("Введіть градуси: "))
-    degrees_Type = int(input("Введіть вид градусу: "))
-
-    if degrees_Type == 1:
-        print(theDegrees * 1.8 + 32, "Фарантгейтів")
-        print(theDegrees + 273.15, "Кельвінів")
-    elif degrees_Type == 2:
-        print(theDegrees - 273.15, "Цельсіїв")
-        print((theDegrees - 273.15) * 1.8000 + 32.00, "Фарантгейти")
-    else:
-        print(theDegrees - 32 / 1.8, "Цельсіїв")
-        print(theDegrees - 32 / 1.8 + 273.75, "Кельвінів")
+        print(f"{i} --- {total_debt}")
+    print(f"{user_max_debt} --- {max_debt}")
