302ab1e882e0bad2eee6d139846aa9ad96973966
ilovefish3801 <pipipupi.kom@gmail.com>
2023-07-14 19:24:16 +0300

    a

--- main.py
+++ main.py
@@ -1,87 +1,108 @@
+import json
+
 if __name__ == "__main__":
-    # first_name = str("John")
-    # second_name = str("Doe")
-    # third_name = str("Patrick")
-    #
-    # user_data = {"name": first_name, "fathersName": third_name, "surname": second_name,
-    #              "pib": f"{second_name.title()} {first_name[0].upper()}. {third_name[0].upper()}.", 'age': 26}
-    #
-    # # print(third_name[::-1])
-    #
-    # print(user_data)
-    #
-    # arr_names = ("John", "Jerry", "Simon")
-    #
-    # arr_surnames = {"Doe", "Lee", "Chan"}
-    #
-    # arr_surnames.add('test')
-    # arr_surnames.remove('Lee')
-    #
-    # print(arr_surnames)
+    # arr = ["test!", "test2", "testR"]
 
-    users = [
-        {
-            "name": "John",
-            "age": 31
-        },
-        {
-            "name": "Jane",
-            "age": 27,
-            "family": {
-                'mom': {
-                    "name": "Sophie",
-                    "age": 54,
-                },
-                'dad': {
-                    "name": "Johny",
-                    "age": 56,
-                },
-
-            },
-        },
-        {
-            "name": "Jack",
-            "age": 15
-        },
-        {
-            "name": "Jonathan",
-            "age": 38
-        },
-    ]
-
-    # print(users[2]['name'], users[2]['age'])
-    # print(users[1]["family"])
-
-    # temp_age = users[1]['age']
-    # temp_name = users[1]['name']
+    # count = 0
+    # # 4 ways how to print arr items
+    # for item in arr:
+    #     print(item)
+    # print("___________\n")
     #
-    # if temp_age >= 18:
-    #     print(temp_name, "you are 18 or older congrats !")
-    # elif temp_name <= 18:
-    #     print(temp_name, "you are a child I wont sell you any drinks >:(")
-
-    # degrees = int(input("Введіть температуру: "))
+    # while count < len(arr):
+    #     print(arr[count])
+    #     count += 1
+    # print("___________\n")
     #
-    # if degrees > 0:
-    #     print("Тепло вдягнись легше")
-    # elif degrees < 0:
-    #     print("Ух, як холодно. Вдягни щось тепле")
-    # else:
-    #     print("Вдягнись як хочеш")
+    # for item in range(len(arr)):
+    #     print(arr[item])
+    #
+    # print("___________\n")
+    #
+    # test = [x for x in arr if x == "test!"]
+    # # test 1 and 2 are the same but test 1 is smaller
+    # test_2 = []
+    # for i in arr:
+    #     if i == "test!":
+    #         test_2.append()
 
-    print("""1-Цельсії""")
-    print("""2-Кельвіни""")
-    print("""3-Фарантгейти""")
+    # Sorting and Filtering
 
-    theDegrees = int(input("Введіть градуси: "))
-    degrees_Type = int(input("Введіть вид градусу: "))
+    # numbers = []
+    # for i in range(1, 101, 1):
+    #     numbers.append(i)
+    # print(numbers)
+    #
+    # even_numbers = []
+    # odd_numbers = []
+    # for i in numbers:
+    #     if i % 2 == 0:
+    #         even_numbers.append(i)
+    #     else:
+    #         odd_numbers.append(i)
+    # print(even_numbers)
+    # print(odd_numbers)
 
-    if degrees_Type == 1:
-        print(theDegrees * 1.8 + 32, "Фарантгейтів")
-        print(theDegrees + 273.15, "Кельвінів")
-    elif degrees_Type == 2:
-        print(theDegrees - 273.15, "Цельсіїв")
-        print((theDegrees - 273.15) * 1.8000 + 32.00, "Фарантгейти")
-    else:
-        print(theDegrees - 32 / 1.8, "Цельсіїв")
-        print(theDegrees - 32 / 1.8 + 273.75, "Кельвінів")
+    # sort
+
+    # arr = [4, 56, 2, 6, 3, 89]
+    # arr_two = arr.copy()
+    # flag = True
+    # while flag:
+    #     flag = False
+    #     status_check = True
+    #     # Ефект бульбашки який можна замінити sorted або sort
+    #     for i in range(len(arr) - 1):
+    #         if arr[i] > arr[i + 1]:
+    #             temp = arr[i]
+    #             arr[i] = arr[i + 1]
+    #             arr[i + 1] = temp
+    #     for item in range(len(arr) - 1):
+    #         if arr[item] > arr[item + 1] and flag == False:
+    #             flag = True
+    #
+    # print(arr)
+    # print(sorted(arr_two))
+    #
+    # # sort methods
+    # # ABS
+    # new_val = sorted(arr_two)
+    # print(new_val)
+    # arr_two.sort()
+    # print(arr_two)
+    #
+    # # DESC
+    # new_val_desc = sorted(arr_two, reverse=True)
+    # print(new_val_desc)
+    # arr_two.sort(reverse=True)
+    # print(arr_two)
+    #
+    # str_arr = ["Jonny", "Ann", "Ketty", "Zak"]
+    # # A-Z
+    # sort_az_arr = sorted(str_arr)
+    # print(sort_az_arr)
+    # # Z-A
+    # sort_za_arr = sorted(str_arr, reverse=True)
+    # print(sort_za_arr)
+
+    sort_title = ['hello']
+
+    with open("products.json", "r", encoding="utf-8") as file:
+        products = json.load(file)
+
+        products.sort(key=lambda x: x["title"])
+        print(products)
+        # for item in products:
+        #     title = item['title']
+        #
+        #     sort_title.append(title)
+        #
+        #     sort_title_az = sorted(sort_title)
+        #
+        #     sort_title_za = sorted(sort_title, reverse=True)
+        #
+        # print(sort_title_za)
+        # print(sort_title_az)
+
+    with open("products_az.json", "w") as file:
+        json.dump(products, file)
