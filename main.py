26ad16bb105cfb3846148ac862b3e062b36dc1e4
ilovefish3801 <pipipupi.kom@gmail.com>
2023-07-26 19:58:25 +0300

    a

--- main.py
+++ main.py
@@ -1,87 +1,96 @@
+import json
+
+
+def checkFile():
+    try:
+        with open("zoo.json", "r") as file:
+            zoo = json.load(file)
+    except Exception as err:
+        Arr = {
+            "animals": []
+        }
+        with open("zoo.json", "w") as file:
+            json.dump(Arr, file)
+        checkFile()
+
+
+
+
+
+
+def addAnimal(breed, food):
+    frame = {
+        "breed": breed,
+        "food": food,
+    }
+    zoo['animals'].append(frame)
+    with open("zoo.json", "w", encoding="utf-8") as file:
+        json.dump(zoo, file)
+
+
+def searchAnimal(breed):
+    for i in zoo['animals']:
+        breedList = i['breed']
+        foodList = i['food']
+        if breed.lower() == breedList.lower():
+            return foodList
+
+
+# def numbersSum(firstNum, secondNum):
+#     return firstNum + secondNum
+#
+#
+# def createIntials(name, lastName, fathersName):
+#     initials = False
+#     try:
+#         initials = f"{lastName.capitalize()} {name[0].upper()}. {fathersName[0].upper()}."
+#     except Exception as err:
+#         print("Сталась помилка в створенні ініціалів!")
+#     return initials
+#
+# def runProgram():
+#     numbersSum(1, 2)
+#     print(createIntials("Kurulo", "Kozemiakin", "Ruslanovuc"))
+#     temperature = int(input("Vvedit temperaturu: "))
+#     if temperature > 0:
+#         print("Teplo")
+#     else:
+#         print("Holodno")
+
+
 if __name__ == "__main__":
-    # first_name = str("John")
-    # second_name = str("Doe")
-    # third_name = str("Patrick")
-    #
-    # user_data = {"name": first_name, "fathersName": third_name, "surname": second_name,
-    #              "pib": f"{second_name.title()} {first_name[0].upper()}. {third_name[0].upper()}.", 'age': 26}
-    #
-    # # print(third_name[::-1])
-    #
-    # print(user_data)
-    #
-    # arr_names = ("John", "Jerry", "Simon")
-    #
-    # arr_surnames = {"Doe", "Lee", "Chan"}
-    #
-    # arr_surnames.add('test')
-    # arr_surnames.remove('Lee')
-    #
-    # print(arr_surnames)
+    checkFile()
+    flag = True
+    while flag:
+        print("\n 1 для додавання тварини в зоопарк\n", "2 для знаходження їжі за породою\n",
+              "q щоб вийти з програми")
+        choice = input("Введіть дію: ")
 
-    users = [
-        {
-            "name": "John",
-            "age": 31
-        },
-        {
-            "name": "Jane",
-            "age": 27,
-            "family": {
-                'mom': {
-                    "name": "Sophie",
-                    "age": 54,
-                },
-                'dad': {
-                    "name": "Johny",
-                    "age": 56,
-                },
+        if choice == "1":
+            breed = input("Введіть породу: ")
+            food = input("Введіть рекомендовану їжу для цієї породи ")
+            addAnimal(breed, food)
+        elif choice == "2":
+            search = input("Введіть породу: ")
+            print(searchAnimal(search))
+        else:
+            break
 
-            },
-        },
-        {
-            "name": "Jack",
-            "age": 15
-        },
-        {
-            "name": "Jonathan",
-            "age": 38
-        },
-    ]
+    # active = True
+    # while active:
+    #     try:
+    #         runProgram()
+    #     except Exception as err:
+    #         print(err)
+    #     finally:
+    #         choice = input("Чи бажаєте продовжити користування ? Y/N:")
+    #         if choice.lower() == "n":
+    #             active = False
 
-    # print(users[2]['name'], users[2]['age'])
-    # print(users[1]["family"])
-
-    # temp_age = users[1]['age']
-    # temp_name = users[1]['name']
-    #
-    # if temp_age >= 18:
-    #     print(temp_name, "you are 18 or older congrats !")
-    # elif temp_name <= 18:
-    #     print(temp_name, "you are a child I wont sell you any drinks >:(")
-
-    # degrees = int(input("Введіть температуру: "))
-    #
-    # if degrees > 0:
-    #     print("Тепло вдягнись легше")
-    # elif degrees < 0:
-    #     print("Ух, як холодно. Вдягни щось тепле")
-    # else:
-    #     print("Вдягнись як хочеш")
-
-    print("""1-Цельсії""")
-    print("""2-Кельвіни""")
-    print("""3-Фарантгейти""")
-
-    theDegrees = int(input("Введіть градуси: "))
-    degrees_Type = int(input("Введіть вид градусу: "))
-
-    if degrees_Type == 1:
-        print(theDegrees * 1.8 + 32, "Фарантгейтів")
-        print(theDegrees + 273.15, "Кельвінів")
-    elif degrees_Type == 2:
-        print(theDegrees - 273.15, "Цельсіїв")
-        print((theDegrees - 273.15) * 1.8000 + 32.00, "Фарантгейти")
-    else:
-        print(theDegrees - 32 / 1.8, "Цельсіїв")
-        print(theDegrees - 32 / 1.8 + 273.75, "Кельвінів")
+    # try:
+    #     num = int("test")
+    #     print(num)d
+    # except Exception as err:
+    #     print(err)
+    # finally:
+    #     print("Some text")
