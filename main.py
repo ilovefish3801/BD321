993a4b4cf6aa539b6f473015e22b8a5d5d5bbcd8
ilovefish3801 <pipipupi.kom@gmail.com>
2023-08-21 19:37:59 +0300

    a

--- main.py
+++ main.py
@@ -1,87 +1,104 @@
+import telebot
+from telebot import types
+import requests
+import json
+import pandas as pd
+from pandas import DataFrame
+import openpyxl
+import admins
+import time
+import datetime
+
+baseURL = "https://fakestoreapi.com"
 if __name__ == "__main__":
-    # first_name = str("John")
-    # second_name = str("Doe")
-    # third_name = str("Patrick")
-    #
-    # user_data = {"name": first_name, "fathersName": third_name, "surname": second_name,
-    #              "pib": f"{second_name.title()} {first_name[0].upper()}. {third_name[0].upper()}.", 'age': 26}
-    #
-    # # print(third_name[::-1])
-    #
-    # print(user_data)
-    #
-    # arr_names = ("John", "Jerry", "Simon")
-    #
-    # arr_surnames = {"Doe", "Lee", "Chan"}
-    #
-    # arr_surnames.add('test')
-    # arr_surnames.remove('Lee')
-    #
-    # print(arr_surnames)
+    bot = telebot.TeleBot("")
+    users = {}
 
-    users = [
-        {
-            "name": "John",
-            "age": 31
-        },
-        {
-            "name": "Jane",
-            "age": 27,
-            "family": {
-                'mom': {
-                    "name": "Sophie",
-                    "age": 54,
-                },
-                'dad': {
-                    "name": "Johny",
-                    "age": 56,
-                },
 
-            },
-        },
-        {
-            "name": "Jack",
-            "age": 15
-        },
-        {
-            "name": "Jonathan",
-            "age": 38
-        },
-    ]
 
-    # print(users[2]['name'], users[2]['age'])
-    # print(users[1]["family"])
+    def main_reply_menu():
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row(types.KeyboardButton("Показати продукти"), types.KeyboardButton("Надіслати файл"))
 
-    # temp_age = users[1]['age']
-    # temp_name = users[1]['name']
-    #
-    # if temp_age >= 18:
-    #     print(temp_name, "you are 18 or older congrats !")
-    # elif temp_name <= 18:
-    #     print(temp_name, "you are a child I wont sell you any drinks >:(")
+        return markup
 
-    # degrees = int(input("Введіть температуру: "))
-    #
-    # if degrees > 0:
-    #     print("Тепло вдягнись легше")
-    # elif degrees < 0:
-    #     print("Ух, як холодно. Вдягни щось тепле")
-    # else:
-    #     print("Вдягнись як хочеш")
 
-    print("""1-Цельсії""")
-    print("""2-Кельвіни""")
-    print("""3-Фарантгейти""")
+    def admin_reply_menu():
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row(types.KeyboardButton("Показати 1 продукт"))
 
-    theDegrees = int(input("Введіть градуси: "))
-    degrees_Type = int(input("Введіть вид градусу: "))
+        return markup
 
-    if degrees_Type == 1:
-        print(theDegrees * 1.8 + 32, "Фарантгейтів")
-        print(theDegrees + 273.15, "Кельвінів")
-    elif degrees_Type == 2:
-        print(theDegrees - 273.15, "Цельсіїв")
-        print((theDegrees - 273.15) * 1.8000 + 32.00, "Фарантгейти")
-    else:
-        print(theDegrees - 32 / 1.8, "Цельсіїв")
-        print(theDegrees - 32 / 1.8 + 273.75, "Кельвінів")
+
+    def get_product():
+        response = requests.get(f"{baseURL}/products")
+
+        data = response.json()
+
+        wholePrice = 0
+        text = ''
+        for i in data:
+            price = i['price']
+            title = i['title']
+            id = i['id']
+            text += f"{id}. {title} -- {price}\n"
+        return text
+
+
+    def show_one_product(msg):
+        id = msg.text
+        resp = requests.get(f"{baseURL}/products/{int(id)}")
+        bot_resp = resp.text
+
+        return bot.reply_to(msg, bot_resp)
+
+
+    @bot.message_handler(commands=['start', 'help'])
+    def send_welcome(msg):
+        # bot.reply_to(message, "Howdy, how are you doing?")
+        cid = msg.chat.id
+        bot.send_message(cid, "Hello !", reply_markup=main_reply_menu())
+
+
+    @bot.message_handler(commands=['admin'])
+    def admin_commands(msg):
+        admin = admins.ADMINS
+        cid = msg.chat.id
+        if cid in admin:
+            bot.reply_to(msg, "Admin activated", reply_markup=admin_reply_menu())
+
+
+    @bot.message_handler(func=lambda message: True)
+    def echo_all(msg):
+        encounter = 1
+        admin = admins.ADMINS
+        cid = msg.chat.id
+        if msg.text == "Показати продукти":
+            text = get_product()
+            bot.send_message(cid, text)
+        if msg.text == "Показати 1 продукт" and cid in admin:
+            question = bot.send_message(cid, "Введіть ID продукта: ")
+            bot.register_next_step_handler(question, show_one_product)
+        elif msg.text == "Надіслати файл":
+            file = open("data.xlsx", "rb")
+            bot.send_document(cid, file)
+
+    # bot.infinity_polling()
+# Pandas
+data = {
+    'Name': ["John", "Jane", "Jack"],
+    'Age': [25, 26, 11],
+    'City': ["New York", "LA", "New York"]
+}
+
+df = pd.DataFrame(data)
+
+exel_file_path = f"data{datetime.datetime.now().date()}.xlsx"
+
+df.to_excel(exel_file_path, index=False)
+
+# df = pd.read_excel(exel_file_path)
+
+# Reading excel file (turning it back to dictionary)
+# for _, row in df.iterrows():
+#     print(row['Name'], row['Age'], row['City'])
